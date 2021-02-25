3e12a025e903d2392049c611c41a93f129678d72
Dylann B <31523130+takitsu21@users.noreply.github.com>
2021-02-25 16:40:14 +0100

    Class map #4

---
+++ RogueLikeMADY/src/main/java/com/mady/utils/Map.java
@@ -0,0 +1,64 @@
+package com.mady.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Map {
+    private final int nbSalles;
+    private final Case[][] map;
+    private List<Salle> salles = new ArrayList<>();
+    private final int BASE_HEIGHT = 64;
+    private final int BASE_WIDTH = 64;
+
+    public static void main(String[] args) {
+        Map map = new Map(1);
+        map.createMap();
+        Position pos = map.salles.get(0).getPos();
+        map.map[pos.getX()][pos.getY()] = new Case("@");
+//        System.out.println(Arrays.deepToString(map.salles.get(0)));
+
+        System.out.println();
+        System.out.println(map.toString());
+    }
+
+    public Map(int nbSalles) {
+        this.nbSalles = nbSalles;
+        this.map = new Case[BASE_WIDTH][BASE_HEIGHT];
+    }
+
+    private void createMap() {
+        for (int i = 0; i < BASE_WIDTH; i++) {
+            for (int j = 0; j < BASE_HEIGHT; j++) {
+                map[i][j] = new Case(".");
+            }
+        }
+        generateRooms();
+    }
+
+    private void generateRoom(int x, int y) {
+        Salle s = new Salle(new Position(x, y));
+
+        for (int i = 0; i < s.getLargeur(); i++) {
+            for (int j = 0; j < s.getHauteur(); j++) {
+                map[i + y][j + y] = s.getRepresentation()[i][j];
+            }
+        }
+        salles.add(s);
+    }
+
+    private void generateRooms() {
+        generateRoom(10, 10);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < BASE_WIDTH; i++) {
+            for (int j = 0; j < BASE_HEIGHT; j++) {
+                sb.append(map[i][j].toString());
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
--- RogueLikeMADY/src/main/java/com/mady/utils/Salle.java
+++ RogueLikeMADY/src/main/java/com/mady/utils/Salle.java
@@ -1,79 +1,108 @@
 package com.mady.utils;
 
-import com.mady.utils.Position;
-
 public class Salle {
     private final int largeur;
     private final int hauteur;
-    private int monster_number;
-    private int object_number;
+    private final Position pos;
+    private final int monsterNumber;
+    private final int objectNumber;
     private Case[][] representation;
 
 
-    Salle(int largeur , int hauteur, int monster_number, int object_number){
+    public Salle(int largeur,
+                 int hauteur,
+                 int monsterNumber,
+                 int objectNumber,
+                 Position pos) {
         this.largeur = largeur;
         this.hauteur = hauteur;
-        this.monster_number = monster_number;
-        this.object_number = object_number;
+        this.monsterNumber = monsterNumber;
+        this.objectNumber = objectNumber;
+        this.pos = pos;
         representation = new Case[largeur][hauteur];
-        add_monster();
-        add_item();
+        createSalle(largeur, hauteur);
+        addMonster();
+        addItem();
+    }
+
+    public Salle(int largeur, int hauteur, Position pos) {
+        this(largeur, hauteur, 0, 0, pos);
+    }
+
+    public Salle(Position pos) {
+        this(10, 15, 0, 0, pos);
     }
 
     public Case[][] getRepresentation() {
         return representation;
     }
 
-    private Position getFreePos (){
+    private Position getFreePos() {
         int x = (int) (Math.random() * largeur);
         int y = (int) (Math.random() * largeur);
-        while (!representation[x][y].isOccupied() && !representation[x][y].isWall()){
-             x = (int) (Math.random() * largeur);
-             y = (int) (Math.random() * largeur);
+        while (!representation[x][y].isOccupied() && !representation[x][y].isWall()) {
+            x = (int) (Math.random() * largeur);
+            y = (int) (Math.random() * largeur);
         }
-        return new Position(x,y);
+        return new Position(x, y);
     }
 
-    private void add_monster(){
-        for (int i=0; i<monster_number; i++){
+    private void addMonster() {
+        for (int i = 0; i < monsterNumber; i++) {
             Position pos = getFreePos();
-
-            representation[pos.getX()][pos.getY()].setItem(new Monster());
-
+            representation[pos.getX()][pos.getY()].setItem(new Object());
         }
     }
 
-    private void add_item(){
-        for (int i=0; i<object_number; i++){
+    private void addItem() {
+        for (int i = 0; i < objectNumber; i++) {
             Position pos = getFreePos();
-
-            representation[pos.getX()][pos.getY()].setItem(new Item());
-
+            representation[pos.getX()][pos.getY()].setItem(new Object());
         }
     }
 
+    private void createSalle(int l, int h) {
+        for (int j = 0; j < largeur; j++) {
+            representation[j][0] = new Case("#", null);
 
-    private void create_salle(int l, int h){
-        for(int j = 0 ; j < largeur; j++){
-            representation[j][0] = new Case("#",null);
         }
-        for(int i = 1 ; i < hauteur-1; i++){
-            representation[0][i] = new Case("#",null);
-            for(int k = 1; k < largeur-1; k ++){
-                representation[k][i] = new Case(" ",null);
+        for (int i = 1; i < hauteur - 1; i++) {
+            representation[0][i] = new Case("#", null);
+            for (int k = 1; k < largeur - 1; k++) {
+                representation[k][i] = new Case(" ", null);
             }
-            representation[largeur][i] = new Case("#",null);
+            representation[largeur - 1][i] = new Case("#", null);
         }
-        for(int j = 0 ; j < largeur; j++){
-            representation[j][hauteur] = new Case("#",null);
+        for (int j = 0; j < largeur; j++) {
+            representation[j][hauteur - 1] = new Case("#", null);
         }
+    }
+
+    public Position getPos() {
+        return pos;
+    }
+
+    public int getLargeur() {
+        return largeur;
+    }
+
+    public int getHauteur() {
+        return hauteur;
+    }
+
+    public int getmonsterNumber() {
+        return monsterNumber;
+    }
+
+    public int getobjectNumber() {
+        return objectNumber;
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0 ; i < largeur; i++){
-            for(int j = 0 ; j < hauteur; j ++ ){
+        for (int i = 0; i < largeur; i++) {
+            for (int j = 0; j < hauteur; j++) {
                 sb.append(representation[i][j].toString());
             }
             sb.append("\n");
