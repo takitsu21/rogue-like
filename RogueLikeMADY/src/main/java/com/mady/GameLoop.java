63929fd826e8529e2f3decb2405be62748c126e3
MonsieurCo <48797321+MonsieurCo@users.noreply.github.com>
2021-04-25 20:07:43 +0200

    icon added
    Signed-off-by: MonsieurCo <48797321+MonsieurCo@users.noreply.github.com>

--- RogueLikeMADY/src/main/java/com/mady/GameLoop.java
+++ RogueLikeMADY/src/main/java/com/mady/GameLoop.java
@@ -3,8 +3,10 @@ package com.mady;
 import com.mady.utils.*;
 import com.mady.utils.listener.MoveListener;
 
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -41,6 +43,8 @@ public abstract class GameLoop {
         map.addPlayerToMap(controller.getPlayer());
         map.addEntityItemPortal();
         render();
+        ImageIcon img = new ImageIcon(Objects.requireNonNull(getClass().getResource("/MAD16x16.png")));
+        frame.getFrame().setIconImage(img.getImage());
         frame.getFrame().addKeyListener(new MoveListener(map));
         status = GameStatus.STOPPED;
     }
